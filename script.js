ca9df9631385dfde6cc85280f12a1658877fa553
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-08 13:39:40 +0100

    added function to adjust canvas given user input

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
-let viewportWidth = Math.floor(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 0.3)
+let canvasDimensions = calculateCanvasDimensions('large')
 
-let viewportHeight  = Math.floor(Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 0.3)
+generateCanvas(canvasDimensions.width, canvasDimensions.height)
+
+function calculateCanvasDimensions(selectedCanvasSize) {
+
+    // https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript <- see resource
+
+    let viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+
+    let viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+    if (selectedCanvasSize === 'small') {
+
+        viewportWidth = Math.floor(viewportWidth * 0.2)
+
+        viewportHeight = Math.floor(viewportHeight * 0.2)
+
+    } else if (selectedCanvasSize === 'large') {
+
+        viewportWidth = Math.floor(viewportWidth * 0.4)
+
+        viewportHeight = Math.floor(viewportHeight * 0.4)
+
+    } else {
+
+        viewportWidth = Math.floor(viewportWidth * 0.3)
+
+        viewportHeight = Math.floor(viewportHeight * 0.3)
+
+    }
+
+    return {width: viewportWidth, height: viewportHeight}
+
+}
 
 
-console.log('row:' + viewportWidth)
-console.log('column:' + viewportHeight)
-
-generateCanvas(viewportHeight, viewportWidth)
-
-// https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript <- see resource
-
-
-
-function generateCanvas(height, width) {
+function generateCanvas(width, height) {
 
     let canvasWidth = '<div class="pixel"></div>'.repeat(width)
 
