afabae9205b777bc499de83e1c7c33b1ec7ff2bd
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-08 15:21:59 +0100

    Added the ability to change the paint color

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
 generateCanvas(60, 100)
 
+draw('black')
+
+changecolor()
 
 
+
+function changecolor() {
+    document.querySelector('#colorWheel').addEventListener('input', function (e) {
+        let color = e.target.value
+        console.log(color)
+        draw(color)
+    })
+}
 
 function generateCanvas(row, column) {
 
@@ -16,37 +27,43 @@ function generateCanvas(row, column) {
 
 }
 
-document.querySelector('html').addEventListener('mousedown', function () {
-    clickdown = true
-})
+function draw(color) {
 
-document.querySelector('html').addEventListener('mouseup', function () {
-    clickdown = false
-})
-
-document.querySelector('.canvas').addEventListener('mouseleave', function () {
-    painting = false
-})
-
-document.querySelector('.canvas').addEventListener('mouseenter', function () {
-    if (clickdown === true) {
-        painting = true
-    }
-})
-
-document.querySelectorAll('.pixel').forEach(pixel => {
-    pixel.addEventListener('mousedown', function() {
-        painting = true
-        this.style.backgroundColor = "black"
+    document.querySelector('html').addEventListener('mousedown', function () {
+        clickdown = true
     })
-    pixel.addEventListener('mousemove', function() {
-        if (painting === true && clickdown === true) {
-            this.style.backgroundColor = "black"
-        }
+
+    document.querySelector('html').addEventListener('mouseup', function () {
+        clickdown = false
     })
-    pixel.addEventListener('mouseup', function() {
+
+    document.querySelector('.canvas').addEventListener('mouseleave', function () {
         painting = false
     })
-})
+
+    document.querySelector('.canvas').addEventListener('mouseenter', function () {
+        if (clickdown === true) {
+            painting = true
+        }
+    })
+
+    document.querySelectorAll('.pixel').forEach(pixel => {
+        pixel.addEventListener('mousedown', function() {
+            painting = true
+            this.style.backgroundColor = color
+        })
+        pixel.addEventListener('mousemove', function() {
+            if (painting === true && clickdown === true) {
+                this.style.backgroundColor = color
+            }
+        })
+        pixel.addEventListener('mouseup', function() {
+            painting = false
+        })
+    })
+
+
+}
+
 
 
