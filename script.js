0160f9393720519c87f8aa92d272c98ac48685d6
James Taylor <taylor-j12@hotmail.com>
2019-10-08 10:36:16 +0100

    attempting to make buttons that change after you click them

---
+++ script.js
@@ -0,0 +1,21 @@
+let drawing = document.querySelector('#drawing')
+// if(drawing.style.backgroundColor = '#ffffff'){
+//     drawing.addEventListener('click', function () {
+//     drawing.style.backgroundColor = '#a9a9a9'
+//     })
+// }
+// if(drawing.style.backgroundColor = '#a9a9a9'){
+//     drawing.addEventListener('click', function () {
+//         drawing.style.backgroundColor = '#ffffff'
+//     })
+// }
+
+
+
+drawing.addEventListener('click', function () {
+    if(drawing.style.backgroundColor = '#ffffff'){
+        drawing.style.backgroundColor = '#a9a9a9'
+    }else if(drawing.style.backgroundColor = '#a9a9a9'){
+        drawing.style.backgroundColor = '#ffffff'
+    }
+})
