e59d6f4a2e9a4639d36583b8391ae285cea17492
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-08 12:53:39 +0100

    updated canvas so is centered within panel, rather than total window

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 
-generateCanvas(280, 500)
+let viewportWidth = Math.floor(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 0.3)
+
+let viewportHeight  = Math.floor(Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 0.3)
+
+
+console.log('row:' + viewportWidth)
+console.log('column:' + viewportHeight)
+
+generateCanvas(viewportHeight, viewportWidth)
 
 // https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript <- see resource
 
-let viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-
-let viewportHeight  = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
 
+function generateCanvas(height, width) {
 
-function generateCanvas(row, column) {
+    let canvasWidth = '<div class="pixel"></div>'.repeat(width)
 
-    let canvasRow = '<div class="pixel"></div>'.repeat(column)
+    canvasWidth = '<div class="row">' + canvasWidth + '</div>'
 
-    canvasRow = '<div class="row">' + canvasRow + '</div>'
-
-    let canvas = canvasRow.repeat(row)
+    let canvas = canvasWidth.repeat(height)
 
     document.querySelector('.canvas').innerHTML = canvas
 
