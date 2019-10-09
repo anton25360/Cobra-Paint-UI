c8cada63224391ce7d46a82bd75b10e70931c11b
harrycoulton <hfcoulton93@gmail.com>
2019-10-09 10:34:57 +0100

    FINALLY FIXED THE MESS

--- JS/script.js
+++ JS/script.js
@@ -58,4 +58,16 @@ document.querySelectorAll('.pixel').forEach(function(pixel) {
     })
 })
 
+let drawer = document.querySelector('#drawer')
+let eraser = document.querySelector('#eraser')
+
+drawer.addEventListener('click', function () {
+    drawer.style.backgroundColor = '#a9a9a9'
+    eraser.style.backgroundColor = '#ffffff'
+})
+
+eraser.addEventListener('click', function () {
+    eraser.style.backgroundColor = '#a9a9a9'
+    drawer.style.backgroundColor = '#ffffff'
+})
 
