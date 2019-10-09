d8111188547a2e7a79ca1ab14f8cff89819063b6
georginaw <watson.georgina@gmail.com>
2019-10-09 16:28:04 +0100

    sorting out slow function

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
+let color = '#000000'
 
 generateCanvas(60, 100)
-
-draw('#000000')
 
 clickColor()
 
@@ -21,8 +20,7 @@ function clickColor() {
 
 function changeColor() {
     document.querySelector('#colorWheel').addEventListener('input', function(e) {
-        let color = e.target.value
-        draw(color)
+        color = e.target.value
     })
 }
 
@@ -30,7 +28,7 @@ function erase() {
     document.querySelector('#eraser').addEventListener('click', function() {
         document.querySelector('#eraser').style.backgroundColor = '#a9a9a9'
         document.querySelector('#drawer').style.backgroundColor = '#ffffff'
-        draw('#ffffff')
+        color = '#ffffff'
     })
 }
 
@@ -46,40 +44,45 @@ function generateCanvas(row, column) {
 
 }
 
-function draw(color) {
-    let clickdown, painting
 
-    document.querySelector('html').addEventListener('mousedown', function(e) {
-        clickdown = true
-        e.preventDefault()
+
+let clickdown, painting
+
+document.querySelector('html').addEventListener('mousedown', function(e) {
+    clickdown = true
+    e.preventDefault()
+})
+
+document.querySelector('html').addEventListener('mouseup', function() {
+    clickdown = false
+})
+
+document.querySelector('.canvas').addEventListener('mouseleave', function() {
+    painting = false
+})
+
+document.querySelector('.canvas').addEventListener('mouseenter', function() {
+    if (clickdown === true) {
+        painting = true
+    }
+})
+
+
+document.querySelectorAll('.pixel').forEach(function(pixel) {
+    pixel.addEventListener('mousedown', function() {
+        painting = true
+        pixel.style.backgroundColor = color
     })
 
-    document.querySelector('html').addEventListener('mouseup', function() {
-        clickdown = false
-    })
-
-    document.querySelector('.canvas').addEventListener('mouseleave', function() {
-        painting = false
-    })
-
-    document.querySelector('.canvas').addEventListener('mouseenter', function() {
-        if (clickdown === true) {
-            painting = true
+    pixel.addEventListener('mousemove', function() {
+        if (painting === true && clickdown === true) {
+            pixel.style.backgroundColor = color
         }
     })
 
-    document.querySelectorAll('.pixel').forEach(function(pixel) {
-        pixel.addEventListener('mousedown', function() {
-            painting = true
-            this.style.backgroundColor = color
-        })
-        pixel.addEventListener('mousemove', function() {
-            if (painting === true && clickdown === true) {
-                this.style.backgroundColor = color
-            }
-        })
-        pixel.addEventListener('mouseup', function() {
-            painting = false
-        })
+    pixel.addEventListener('mouseup', function() {
+        painting = false
     })
-}
+})
+
+
