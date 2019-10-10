06c8c7eac3f603acdbea2d79d2b8da39211dc499
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-10 09:25:25 +0100

    made formatting changes as per mike comments and added explanation for grabbing viewport

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+
 let color = '#000000'
 const activeTool = '#a9a9a9'
 const inactiveTool = '#ffffff'
@@ -35,28 +36,28 @@ generateCanvas(canvasDimensions.width, canvasDimensions.height)
 
 function calculateCanvasDimensions(selectedCanvasSize) {
 
+    // the window.innerWidth/window.innerHeight properties contain the viewport width (including scroll bars), this
+    // is what we need for rescaling, however when using initial-scale (in the meta tag) the values of
+    // window.innerWidth/window.innerHeight can wrongly scale down to the 'visual viewport' (i.e. the part of the page
+    // the user can see, which may change when scrolling)
+    // if window.innerWidth/window.innerHeight is undefined then use 0
+    // the document.documentElement.clientWidth/document.documentElement.clientHeight properties contain the viewport
+    // width (excluding scroll bars), therefore we want to take the maximum to ensure we either take the
+    // viewport width (including scroll bars) or, if not available, the viewport width (excluding scroll bars)
+
     let viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
 
     let viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
     if (selectedCanvasSize === 'small') {
-
         viewportWidth = Math.floor(viewportWidth * 0.05)
-
         viewportHeight = Math.floor(viewportHeight * 0.05)
-
     } else if (selectedCanvasSize === 'big') {
-
         viewportWidth = Math.floor(viewportWidth * 0.075)
-
         viewportHeight = Math.floor(viewportHeight * 0.075)
-
     } else {
-
         viewportWidth = Math.floor(viewportWidth * 0.06)
-
         viewportHeight = Math.floor(viewportHeight * 0.06)
-
     }
 
     return { width: viewportWidth, height: viewportHeight }
@@ -64,15 +65,10 @@ function calculateCanvasDimensions(selectedCanvasSize) {
 }
 
 function generateCanvas(width, height) {
-
     let canvasWidth = '<div class="pixel"></div>'.repeat(width)
-
     canvasWidth = '<div class="row">' + canvasWidth + '</div>'
-
     let canvas = canvasWidth.repeat(height)
-
     document.querySelector('.canvas').innerHTML = canvas
-
 }
 
 let clickdown, painting
@@ -91,9 +87,7 @@ document.querySelector('.canvas').addEventListener('mouseleave', function() {
 })
 
 document.querySelector('.canvas').addEventListener('mouseenter', function() {
-
     if (clickdown === true) {
-
         painting = true
     }
 })
