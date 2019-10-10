ce3d5079572e8066dcc8a640e75e036067776a24
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-10-10 10:38:15 +0100

    updated according to mike comments - removed unneccassry console.log and ensured the size cookie never expires

--- JS/cookie.js
+++ JS/cookie.js
@@ -1,4 +1,4 @@
 document.querySelector('#formSize').addEventListener('submit', function() {
     let value = document.querySelector('input[name="size"]:checked').value;
-    document.cookie = "size="+value+"; expires=Thu, 18 Dec 2050 12:00:00 UTC";
+    document.cookie = "size="+value+"; expires=0";
 })
